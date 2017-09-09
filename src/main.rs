ff2e562229b3a8a493c550669b7e577808358d4b
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-09-08 21:33:56 -0500

    Switched to termion

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-extern crate getch;
+extern crate termion;
 
 use std::io::Write;
+use std::ops::Deref;
+use termion::input::TermReadEventsAndRaw;
+use termion::event::{Event, Key};
+use termion::raw::IntoRawMode;
 
 fn main() {
     let mut stdout = std::io::stdout();
-    let stdin = getch::Getch::new();
 
-    loop {
-        let byte = stdin.getch().unwrap();
+    let tty = termion::get_tty()
+        .expect("Could not open handle to tty!")
+        .into_raw_mode()
+        .unwrap();
+    let raw_events = TermReadEventsAndRaw::events_and_raw(tty.deref());
+    let mut ctrl_c = false;
 
-        write!(stdout, "0x{:x} ", byte).unwrap();
-        stdout.flush().unwrap();
+    for (event, codes) in raw_events.map(|e| e.unwrap()) {
+        match event {
+            Event::Key(key) => {
+                //abort on 
+                if key == Key::Ctrl('c') && ctrl_c {
+                    break;
+                } else if key == Key::Ctrl('c') {
+                    ctrl_c = true;
+                } else {
+                    ctrl_c = false;
+                }
+
+                for c in codes {
+                    write!(stdout, "0x{:x},", c).unwrap();
+                }
+                //replace the last comma with a space
+                stdout.write(&[b'\x08', b' ']).unwrap();
+                stdout.flush().unwrap();
+            }
+            //ignore non-keyboard events
+            _ => {}
+        };
+
     }
 }
